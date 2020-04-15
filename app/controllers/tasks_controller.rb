92fa36608f6c268ebbe747078974855d77c533ae
masato.takekoshi <koshitake1@gmail.com>
2020-04-15 11:03:26 +0000

    complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -73,4 +73,8 @@ class TasksController < ApplicationController
       redirect_to root_url
     end
   end
+  
+   def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
 end
