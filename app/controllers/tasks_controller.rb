3d2a24a92ad1bb0ce6e0936f4ecb578909579557
masato.takekoshi <koshitake1@gmail.com>
2020-04-15 12:32:20 +0000

    a

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,13 @@
 class TasksController < ApplicationController
    
     before_action :require_user_logged_in
-    before_action :correct_user, 
-    only: [:destroy, :updata, :show, :edit ]
+    before_action :correct_user, only: [:destroy, :updata, :show, :edit ]
     
-    before_action :current_user,
-    only:  [:destroy, :updata, :show, :edit]
     def index
-        
         @tasks = current_user.tasks.order(id: :desc)
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
@@ -21,28 +16,24 @@ class TasksController < ApplicationController
     
     def create
         @task = current_user.tasks.build(task_params)
-    if @task.save
-      flash[:success] = 'タスクを投稿しました。'
-      redirect_to root_url
-    else
-      @tasks = current_user.tasks.order(id: :desc)
-      flash.now[:danger] = 'タスクの投稿に失敗しました。'
-      render 'tasks/new'
+        if @task.save
+            flash[:success] = 'タスクを投稿しました。'
+            redirect_to root_url
+        else
+            @tasks = current_user.tasks.order(id: :desc)
+            flash.now[:danger] = 'タスクの投稿に失敗しました。'
+            render 'tasks/new'
         end
     end
     
     def edit
-        @task = Task.find(params[:id])
     end
     
     def update
-         
-          @task = Task.find(params[:id])
-          
+        @task = Task.find(params[:id])
         if @task.update(task_params)
             flash[:success] = "Task は正常に更新されました"
             redirect_to @task
-            
         else
             flash.now[:danger] = "Task は更新されませんでした"
             render :edit
@@ -50,31 +41,22 @@ class TasksController < ApplicationController
     end
     
     def destroy
-         @task = Task.find(params[:id])
-         
         @task.destroy
         flash[:success] = "Task は正常に削除されました"
         redirect_to tasks_path
     end
     
-    
     private
-    
-    
     
     def task_params
         params.require(:task).permit(:content, :status)
     end
     
-    
-     def correct_user
+    def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
     end
   end
   
-   def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
   
+  root to: "tasks#index"
   
-
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   
+  get "signup", to: "users#new"
+  resources :users, only: [ :new, :create]
   
-  get 'users/new'
-  
-    root to: "tasks#index"
-    
-    get "login", to: "sessions#new"
-    post "login", to: "sessions#create"
-    delete "logout", to: "sessions#destroy"
-    
-    
-    get "signup", to: "users#new"
-    resources :users, only: [ :new, :create]
-    
-    resources :tasks
+  resources :tasks
 end
